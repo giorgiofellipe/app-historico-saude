36b9fe30136ab35d2bc0b8d0be22811455843830
= <=>
2015-07-29 09:47:38 -0300

    Altera escopo da variável app controller

--- app/www/states/doenca/doenca.controller.js
+++ app/www/states/doenca/doenca.controller.js
@@ -3,12 +3,12 @@
   angular.module('historico-saude.state.doenca.controller', [])
     .controller('DoencaController', Doenca);
 
-  function Doenca($scope, $state, $http) {
+  function Doenca($scope, $state, $http, $rootScope) {
       $scope.doenca = {};
 
       $scope.save = function () {
         $scope.doenca.data = new Date();
-        $http.post($scope.serverUrl + '/doenca', $scope.doenca)
+        $http.post($rootScope.serverUrl + '/doenca', $scope.doenca)
           .success(function(){
             $state.go('app.doencas');
           })
